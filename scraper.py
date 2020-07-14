004ce09f42e563d2729d47b44559a1f5733d52ef
gatsu420 <istifajar.he@gmail.com>
2020-07-14 15:21:29 +0000

    add db read+write and config scraped elements

--- scraper.py
+++ scraper.py
@@ -5,16 +5,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import TimeoutException
 from datetime import datetime
 import os
 import pymysql
 import time
 import secrets
+import re
 
+start_time = datetime.now()
+
+keywords = []
 product_names = []
 product_links = []
+product_links_tidy = []
+product_reviewcounts = []
+product_soldcounts = []
+product_viewcounts = []
+keyword_ids = []
+keyword_ids_tidy = []
+
+try:
+    conn = pymysql.connect(host = os.environ['HAKASETEST_HOST'], 
+                            user = os.environ['HAKASETEST_USER'],
+                            password = os.environ['HAKASETEST_PASS'])
+    cur = conn.cursor()
+    cur.execute('select * from {TP_KEYWORDS}'.format(TP_KEYWORDS=os.environ['TP_KEYWORDS']))
+    keywords_raw = cur.fetchall()
+    conn.close()
+except:
+    print('fail to read {TP_KEYWORDS}'.format(TP_KEYWORDS=os.environ['TP_KEYWORDS']))
 
 display = Display(visible = 0, size = (800, 600))
 display.start()
@@ -24,29 +44,70 @@ user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36
 opts = Options()
 opts.add_argument('--disable-notifications')
 opts.add_argument(f'user-agent={user_agent}')
-#opts.add_argument('--headless') #no need to invoke headless due to running in linux ec2 environment
-opts.add_argument('--no-sandbox') #need to invoke no sandbox due to running in linux ec2 environment
-
+#opts.add_argument('--headless') #no need to invoke headless due to running in gce environment
+opts.add_argument('--no-sandbox') #need to invoke no sandbox due to running in gce environment
 driver = webdriver.Chrome(options = opts)
-driver.get('https://www.tokopedia.com/search?st=product&ob=5&q=kaos%20polos')
-time.sleep(secrets.choice(range(5, 16)))
-product_link_elements = driver.find_elements_by_class_name('css-89jnbj')
 
-for i in range(len(product_link_elements)):
-    product_links.append(product_link_elements[i].get_attribute('href'))
+for i in range(len(keywords_raw)):
+    keywords.append(keywords_raw[i][1].replace(' ', '%20'))
+    keyword_ids.append(keywords_raw[i][0])
 
-for j in range(1, 11):
-    driver.get(product_links[j])
-    time.sleep(8)
+for j in range(len(keywords)):
+    driver.get('https://www.tokopedia.com/search?st=product&ob=5&q=' + keywords[j])
+    time.sleep(secrets.choice(range(5, 11)))
+    product_link_elements = driver.find_elements_by_class_name('css-89jnbj')
+
+    for ji in range(len(product_link_elements)):
+        if 'https://ta.tokopedia.com' not in str(product_link_elements[ji].get_attribute('href')):
+            product_links.append(product_link_elements[ji].get_attribute('href'))
+            keyword_ids_tidy.append(keyword_ids[j])
+
+for k in range(len(product_links)):
+    driver.get(product_links[k])
+    time.sleep(secrets.choice(range(3, 7)))
+    #driver.save_screenshot('error_screenshot.png') #capture screenshot for element debugging
 
     product_name = driver.find_element_by_class_name('css-x7lc0h').text 
     product_names.append(product_name)
     
-    product_link = driver.current_url.split('?', 1)[0]
-    product_links.append(product_link)
-    
-    driver.save_screenshot('error_screenshot.png')
+    product_link_tidy = driver.current_url.split('?', 1)[0]
+    product_links_tidy.append(product_link_tidy)
+
+    try:
+        product_reviewcount = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[3]/span[1]/span[2]').text
+        product_reviewcounts.append(''.join(re.findall('[0-9]', product_reviewcount)))
+    except NoSuchElementException:
+        product_reviewcounts.append(None)
+
+    try:
+        product_soldcount = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[3]/span[2]/b/span').text 
+        product_soldcounts.append(''.join(re.findall('[0-9]', product_soldcount)))
+    except NoSuchElementException:
+        product_soldcounts.append(None)
+
+    try:
+        product_viewcount = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[3]/span[3]/b').text 
+        product_viewcounts.append(''.join(re.findall('[0-9]', product_viewcount)))
+    except NoSuchElementException:
+        product_viewcounts.append(None)
+
+inserted_time = [str(datetime.now())] * len(product_names)
+
+try:
+    conn = pymysql.connect(host = os.environ['HAKASETEST_HOST'], 
+                            user = os.environ['HAKASETEST_USER'],
+                            password = os.environ['HAKASETEST_PASS'])
+    cur = conn.cursor()
+    query = '''insert into {TP_RECENT_UPDATE} 
+            (keyword_id, product_name, product_link, sold_count, review_count, view_count, inserted_time) values (%s, %s, %s, %s, %s, %s, %s)
+            '''.format(TP_RECENT_UPDATE=os.environ['TP_RECENT_UPDATE'])
+    recent_update = list(zip(keyword_ids_tidy, product_names, product_links_tidy, product_soldcounts, product_reviewcounts, product_viewcounts, inserted_time))
+    cur.executemany(query, recent_update)
+    conn.commit()
+    conn.close()
+except:
+    print('fail to write {TP_RECENT_UPDATE}'.format(TP_RECENT_UPDATE=os.environ['TP_RECENT_UPDATE']))
 
 os.system('killall -9 Xvfb chromedriver')
-print(product_names)
-print(product_links)
+end_time = datetime.now()
+print(end_time - start_time) #get script runtime
